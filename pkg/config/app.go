26585d89dd7d84b6ed536197da042042d8efb5d5
Deatsilence <mert_im2000@hotmail.com>
2023-12-03 03:24:43 +0300

    Update database connection credentials

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "mertdogan:Sylar3120@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "root:TheSylar3120@/simplerest?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
 		panic(err)
